36b4c0a2988a921aa7e78172e7702e2930a32878
AngelaEspinosa <titagena@gmail.com>
2022-05-16 12:01:48 -0500

    COMMAND: se logra crear el comando de agregarDirectrizDeMentoria con sus atributos, contructor y metodos gettrer.

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/Curso.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/Curso.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+
 public class Curso extends AggregateEvent<CursoId> {
     protected Nombre nombre;
     protected Descripcion descripcion;
--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizDeMentoria.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizDeMentoria.java
@@ -1,2 +1,23 @@
-package co.com.sofkau.entrenamiento.curso.commands;public class AgregarDirectrizDeMentoria {
+package co.com.sofkau.entrenamiento.curso.commands;
+
+import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.Directiz;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+
+public class AgregarDirectrizDeMentoria extends Command {
+    private final MentoriaId mentoriaId;
+    private final Directiz directiz;
+
+    public AgregarDirectrizDeMentoria(MentoriaId mentoriaId, Directiz directiz) {
+        this.mentoriaId = mentoriaId;
+        this.directiz = directiz;
+    }
+
+    public MentoriaId getMentoriaId() {
+        return mentoriaId;
+    }
+
+    public Directiz getDirectiz() {
+        return directiz;
+    }
 }
