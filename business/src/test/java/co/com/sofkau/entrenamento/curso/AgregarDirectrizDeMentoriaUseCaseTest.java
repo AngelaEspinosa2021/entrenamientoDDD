e17b45a041175cd7d3fd28cc34ccf03912695fc0
AngelaEspinosa <titagena@gmail.com>
2022-05-16 13:37:41 -0500

    TEST: se establecen las validaciones necesarias a los objetos de valor MentoriaID y Directriz, para poder ser llenados desde el test.

--- business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCaseTest.java
+++ business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCaseTest.java
@@ -1,4 +1,30 @@
+package co.com.sofkau.entrenamento.curso;
+
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizDeMentoria;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
+import co.com.sofkau.entrenamiento.curso.values.Directiz;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
 class AgregarDirectrizDeMentoriaUseCaseTest {
-  
+
+    @InjectMocks
+    private AgregarDirectrizDeMentoriaUseCase useCase;
+
+    @Test
+    void agregarDirectrizAUnaMentoriaHappyPass(){
+        //arrange
+        CursoId cursoId = CursoId.of("0010");
+        MentoriaId mentoriaId = MentoriaId.of("0002");
+        Directiz directiz = new Directiz("Nueva Directriz");
+        var command = new AgregarDirectrizDeMentoria( cursoId, mentoriaId, directiz);
+    }
+
 }
