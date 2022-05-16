8cc5b32725f27e9585ea1efe31126ca4751904fe
AngelaEspinosa <titagena@gmail.com>
2022-05-16 12:11:49 -0500

    USECASE: pendiente la realizacion de los test unitarios.

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizDeMentoria.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizDeMentoria.java
@@ -1,14 +1,17 @@
 package co.com.sofkau.entrenamiento.curso.commands;
 
 import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
 import co.com.sofkau.entrenamiento.curso.values.Directiz;
 import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
 
 public class AgregarDirectrizDeMentoria extends Command {
+    private final CursoId cursoId;
     private final MentoriaId mentoriaId;
     private final Directiz directiz;
 
-    public AgregarDirectrizDeMentoria(MentoriaId mentoriaId, Directiz directiz) {
+    public AgregarDirectrizDeMentoria(CursoId cursoId, MentoriaId mentoriaId, Directiz directiz) {
+        this.cursoId = cursoId;
         this.mentoriaId = mentoriaId;
         this.directiz = directiz;
     }
@@ -19,5 +22,9 @@ public class AgregarDirectrizDeMentoria extends Command {
 
     public Directiz getDirectiz() {
         return directiz;
+    }
+
+    public CursoId getCursoId() {
+        return cursoId;
     }
 }
